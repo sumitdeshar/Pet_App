67968b248c7b15c07190a6bba034c41fc09c702a
Sumit Deshar <sumeetdsr@gmail.com>
2023-12-16 22:39:38 +0545

    update on profile

---
+++ frontend/lib/Constants/token_auth.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:frontend/Pages/login_page.dart';
+
+final storage = FlutterSecureStorage();
+
+class TokenVerification extends StatefulWidget {
+  const TokenVerification({super.key});
+
+  @override
+  State<TokenVerification> createState() => _TokenVerificationState();
+}
+
+class _TokenVerificationState extends State<TokenVerification> {
+  Future<void> checkAccessToken() async {
+    final String? accessToken = await getAccessToken();
+
+    if (accessToken == null) {
+      // No access token, navigate to login
+      print('NULL');
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LoginPage()));
+    } else {
+      // Check if the token is expired
+      final bool isTokenExpired = await isAccessTokenExpired(accessToken);
+
+      if (isTokenExpired) {
+        // Token expired, try refreshing
+        await refreshAccessToken();
+
+        // Check again after refresh
+        final String? newAccessToken = await getAccessToken();
+        if (newAccessToken == null) {
+          // Refresh failed, navigate to login
+          print('NULL2');
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+        }
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+Future<bool> isAccessTokenExpired(String accessToken) async {
+  try {
+    final Map<String, dynamic> decodedToken = json.decode(
+      utf8.decode(base64Url.decode(
+        accessToken.split('.')[1],
+      )),
+    );
+
+    if (decodedToken.containsKey('exp')) {
+      final int expirationTime = decodedToken['exp'];
+      final int currentTimeInSeconds =
+          DateTime.now().toUtc().millisecondsSinceEpoch ~/ 1000;
+
+      return expirationTime < currentTimeInSeconds;
+    }
+
+    // If the token doesn't have an expiration claim, consider it expired
+    return true;
+  } catch (e) {
+    // Handle decoding errors or missing claims
+    print('Error decoding token: $e');
+    return true;
+  }
+}
+
+Future<String?> getAccessToken() async {
+  // Retrieve the access token from secure storage
+  return await storage.read(key: 'access_token');
+}
+
+Future<void> refreshAccessToken() async {
+  try {
+    final String? refreshToken = await storage.read(key: 'refresh_token');
+
+    if (refreshToken != null) {
+      final response = await http.post(
+        Uri.parse(
+            'http://10.0.2.2/token/refresh'), // Replace with your server's token refresh endpoint
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: {
+          'refresh': refreshToken,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+        final String newAccessToken = responseData['access'];
+
+        // Update the stored access token
+        await storage.write(key: 'access_token', value: newAccessToken);
+
+        print('New Access Token: $newAccessToken');
+      } else {
+        // Handle other refresh token response statuses
+        print('Error refreshing access token: ${response.statusCode}');
+      }
+    } else {
+      // No refresh token found, handle accordingly
+      print('No refresh token found');
+    }
+  } catch (e) {
+    // Handle refresh token request errors
+    print('Error refreshing access token: $e');
+  }
+}
---
+++ frontend/lib/Pages/owner_profile.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class PetOwnerProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pet Owner Profile'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage(
+                    'assets/pet_owner_image.jpg'), // Replace with the actual image path
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Pet Lover',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Proud Owner of a Labrador Retriever',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'About Me:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Passionate about animals and dedicated to providing the best care for my furry friend.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'My Pets:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              _buildPetSegment('Buddy', 'Labrador Retriever'),
+              _buildPetSegment('Whiskers', 'Persian Cat'),
+              SizedBox(height: 16),
+              Text(
+                'Posts:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              _buildPostPreview(
+                'Fun day at the park',
+                'Enjoyed a lovely day outdoors with my furry friend! 🐾',
+                'assets/fun_day_park.jpg',
+              ),
+              _buildPostPreview(
+                'Pet grooming tips',
+                'Sharing some tips for keeping your pets well-groomed. 🛁',
+                'assets/pet_grooming_tips.jpg',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPetSegment(String petName, String petBreed) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: Colors.grey)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            petName,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            petBreed,
+            style: TextStyle(fontSize: 16, color: Colors.grey),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPostPreview(
+      String postTitle, String postPreview, String imagePath) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: Colors.grey)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            postTitle,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 4),
+          Text(
+            postPreview,
+            style: TextStyle(fontSize: 14, color: Colors.grey),
+          ),
+          SizedBox(height: 8),
+          Image.asset(
+            imagePath,
+            height: 150, // Adjust the height as needed
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+  }
+}
